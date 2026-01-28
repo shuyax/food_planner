f5f1cc578cb2c1ff4d598bce7e0404a5089df039
Shuya <syx172524@gmail.com>
2026-01-28 11:19:29 -0500

    Updated the logic that there is only one meal type list that user can create a new meal to a day

--- frontend/src/components/MealTypeList.js
+++ frontend/src/components/MealTypeList.js
@@ -1,24 +1,80 @@
-export function MealTypeList( {mealIndex, mealTypes, meal, updateMeal} ) {
+import { useState } from "react";
+import { createMeal, fetchMealTypes } from "../services/MealService";
+import { useMutation, useQuery } from "@tanstack/react-query";
 
-    function handleMealChange(e) {
-        updateMeal ({
+export function MealTypeList( {mealDate, setCalendarRefresh} ) {
+
+    const defaultMeal = {
+        mealId: -1,
+        mealType: '',
+        mealDate: mealDate,
+    }
+    const [meal, setMeal] = useState(defaultMeal);
+
+    const { data: mealTypesData,
+        isLoading: mealTypesIsLoading,
+        error: mealTypesError } = useQuery({
+        queryKey: ["mealTypes"],
+        queryFn: fetchMealTypes
+    });
+
+    const createMealMutation = useMutation({
+        mutationFn: ({ mealType, mealDate}) =>
+            createMeal(mealType, mealDate),
+        onSuccess: (data) => {
+            console.log("Meal created:" , data);
+            setMeal(prev => ({
+                ...prev,
+                mealId: data,
+            }));
+        },
+        onError: (error) => {
+            console.error("Failed to create meal:", error);
+            alert("Failed to save meal.");
+        }
+    })
+
+    async function handleAddMeal() {
+        if (!meal.mealType || !meal.mealDate) {
+            alert('Must select a valid meal type')
+            return;
+        }
+        try {
+            await createMealMutation.mutateAsync({
+                mealType: meal.mealType,
+                mealDate: meal.mealDate,
+            });
+            console.log("Meal creation triggered!");
+            setCalendarRefresh((prev) => prev + 1);
+            setMeal({...defaultMeal});
+        } catch (err) {
+            console.error("Failed to create meal:", err);
+        }
+    }
+
+    
+    function handleMealChange (e) {
+        setMeal({
             ...meal,
             mealType: e.target.value
         });
     }
 
+    if ( mealTypesIsLoading) return <p>Meal Types Loading ...</p>;
+    if ( mealTypesError) return <p>Meal Types Error: {mealTypesError.message}</p>;
+
     return (<div className="meal-type-list">
-        <label htmlFor={`meal-type-${mealIndex}`}>Meal Type: </label>
+        <label htmlFor={`meal-type`}>Meal Type: </label>
         <select name="meal-type" 
-            id={`meal-type-${mealIndex}`}
+            id={`meal-type`}
             value={meal.mealType} 
-            onChange={handleMealChange}
+            onChange={(e) => handleMealChange(e)}
         >
             <option value="">-- Select a meal type --</option>
-            {mealTypes.map((mealType, index) => (
+            {mealTypesData.map((mealType, index) => (
                 <option key={index} value={mealType}>{mealType}</option>   
             ))}
         </select>
+        <button id="add-meal" type="button" onClick={handleAddMeal}>Add Meal</button>
     </div>)
 };
-
