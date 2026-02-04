88caff2867b324d51ffe90ac98343c13e2064dca
Shuya <syx172524@gmail.com>
2026-02-04 14:55:30 -0500

    Added edit food to the route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import AddFoodForm from './pages/AddFoodForm';
 import AddIngredientForm from './pages/AddIngredientForm';
 import AddMealForm from './pages/AddMealForm';
+import EditFoodForm from './pages/EditFoodForm';
 import Home from './pages/Home'
 import { Routes, Route, Link } from "react-router-dom";
 
@@ -13,12 +14,14 @@ function App() {
       <nav>
         <Link to="/">HOME</Link> |{" "}
         <Link to="/add-ingredient">Add New Ingredient</Link> |{" "}
-        <Link to="/add-food">Add New Food</Link>
+        <Link to="/add-food">Add New Food</Link> 
+        {/* <Link to="/edit-food">Edit Food</Link> */}
       </nav>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/add-ingredient' element={<AddIngredientForm />} />
         <Route path="/add-food" element={<AddFoodForm />} />
+        <Route path="/edit-food/:foodId" element={<EditFoodForm />} />
         <Route path='/add-meal' element={<AddMealForm />} />
        </Routes>
     </div>
