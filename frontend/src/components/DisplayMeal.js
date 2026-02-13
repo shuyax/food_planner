b4193324d5df7608f5da72caf52e202161bca734
Shuya <syx172524@gmail.com>
2026-02-13 16:49:22 -0500

    Removed unused button

--- frontend/src/components/DisplayMeal.js
+++ frontend/src/components/DisplayMeal.js
@@ -3,11 +3,6 @@ function DisplayMeal( {meal, color, setEditingMeal} ) {
         key={`meal-section-${meal.mealType}`} className="meal-section" 
         style={{backgroundColor: color, borderColor: color}}
         onClick={() => setEditingMeal(meal)}>
-        {/* <button onClick={() => setEditingMeal(meal)} id={`meal-edit-btn-${meal.mealType}`} className="meal-edit-btn" title="Edit Meal">
-            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="currentColor">
-                <path d="M3 17.25V21h3.75l11.06-11.06-3.75-3.75L3 17.25zM21.41 6.34a1 1 0 0 0 0-1.41l-2.34-2.34a1 1 0 0 0-1.41 0L15.13 4.34l3.75 3.75 2.53-2.53z"/>
-            </svg>
-        </button> */}
         <strong>{meal.mealType.toUpperCase()}</strong>
         <ol className="meal-foods" id={`meal-foods-${meal.mealType}`}>
             {meal.foods.map(food => 
