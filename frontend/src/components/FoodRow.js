345a5bf543c1446c134d45c7d4283cf92c5beb6b
Shuya <syx172524@gmail.com>
2026-02-13 13:49:25 -0500

    Added the function to filter out the existing meal foods from options to avoid food duplication

--- frontend/src/components/FoodRow.js
+++ frontend/src/components/FoodRow.js
@@ -1,34 +1,35 @@
-export function FoodRow( { existingFoods, food, updateFood, mealDate, mealType, removeFood } ) {
-
+export function FoodRow( { existingFoods, food, updateFood, mealType } ) {
+    const options = food.foodId && food.foodId !== -1 && !existingFoods.some(f => f.foodId === food.foodId)
+        ? [food, ...existingFoods]   // prepend current food if missing
+        : existingFoods;
     function handleFoodChange(e) {
         const selected = existingFoods.find(
-            i => i.name === e.target.value
+            i => i.foodName === e.target.value
         );
         if (!selected) return;
         const updatedFood = {
             ...food,
-            foodId: selected.id,
-            foodName: selected.name,
-            description: selected.description
+            foodId: selected.foodId,
+            foodName: selected.foodName,
+            foodDescription: selected.foodDescription
         }
         updateFood(updatedFood)
     };
-
     return (<>
-        <li key={`${mealDate}-${mealType}-${food.foodId}`}>
-            <label htmlFor={`${mealDate}-${mealType}-${food.foodId}`}></label>
-            <select name={`${mealDate}-${mealType}-${food.foodId}`} 
-                id={`${mealDate}-${mealType}-${food.foodId}`} 
-                value={food.foodName ?? ""} 
-                onChange={(e) => handleFoodChange(e)}
-            >
-                <option value="">-- Select a food --</option>
-                {existingFoods.map((food, i) => (
-                    <option key={`${mealDate}-${mealType}-${food.foodId}-${i}`} value={food.name}>{food.name}</option>   
-                ))}
-            </select>
-            <button id={`remove-${mealDate}-${mealType}-${food.foodId}`} className="remove-food-btn" type="button" onClick={removeFood}>✕</button>
-        </li>
+        <label htmlFor={`${mealType}-${food.foodName}`}></label>
+        <select name={`${mealType}-${food.foodName}`} 
+            className="food-list-select"
+            id={`${mealType}-${food.foodName}`} 
+            value={food.foodName ?? ""} 
+            onChange={(e) => {
+                e.stopPropagation()
+                handleFoodChange(e)}}
+        >
+            <option value="">-- Select a food --</option>
+            {options.map(food => (
+                <option key={`${food.foodId}`} value={food.foodName}>{food.foodName}</option>   
+            ))}
+        </select>
     </>)
 };
 
