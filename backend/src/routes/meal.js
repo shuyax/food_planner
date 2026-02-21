01e66659223eefde1b6de36be6f75b8b290f31c5
Shuya <syx172524@gmail.com>
2026-02-20 22:30:28 -0500

    Corrected the route

--- backend/src/routes/meal.js
+++ backend/src/routes/meal.js
@@ -7,7 +7,7 @@ router.get("/meal-types", MealController.getMealTypes);
 router.post("/", MealController.createMeal)
 router.get("/:mealId/foods", MealController.getRelatedFoods);
 router.post("/update-foods", MealController.updateFoodsToMeal)
-router.post("/create-food", MealController.updateFoodsToMeal)
+router.post("/create-food", MealController.createFoodInMeal)
 router.put("/update-food", MealController.updateFoodInMeal)
 router.delete("/delete/:mealId", MealController.deleteMeal)
 router.delete("/delete-food/:mealFoodId", MealController.deleteMealFood)
