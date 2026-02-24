6188cdeb4143d5b27a3e0583f4a7b48aca2a86d7
Shuya <syx172524@gmail.com>
2026-02-24 16:11:20 -0500

    Fixed the syntax

--- frontend/src/pages/CreateFoodPage.js
+++ frontend/src/pages/CreateFoodPage.js
@@ -91,12 +91,12 @@ function CreateFoodPage( {visibleBackButton = true} ) {
             onChange={(e) => handleTextarea(e)}
         /></label>
         <button id="food-create" onClick={(e) => {e.stopPropagation();
-            handleCreateFood()}} disabled={food.foodName ===""} title="Create Food" aria-label="Create Food" className="create-btn"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="12" y1="12" x2="12" y2="18"></line><line x1="9" y1="15" x2="15" y2="15"></line></svg></button>
+            handleCreateFood()}} disabled={food.foodName ===""} title="Create Food" aria-label="Create Food" className="create-btn"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path><polyline points="14 2 14 8 20 8"></polyline><line x1="12" y1="12" x2="12" y2="18"></line><line x1="9" y1="15" x2="15" y2="15"></line></svg></button>
         {errorNote !== "" && <p>{errorNote}</p>}
         {toastMessage !== "" && <p className="toast-message">{toastMessage}</p>}
         </div>
         {visibleBackButton && <button id="food-back" className="back-btn" title="Back to Home Page" aria-label="Back to Home Page" onClick={(e) => {e.stopPropagation();
-            navigate(`/`)}}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"> <circle cx="12" cy="12" r="10"></circle> <path d="M15 12H9"></path><polyline points="12 15 9 12 12 9"></polyline></svg></button>}
+            navigate(`/`)}}><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"> <circle cx="12" cy="12" r="10"></circle> <path d="M15 12H9"></path><polyline points="12 15 9 12 12 9"></polyline></svg></button>}
     </div></>)
 }
 
