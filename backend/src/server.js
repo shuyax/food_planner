694a118bacb18560f55e86866367eff67980c732
Shuya <syx172524@gmail.com>
2026-02-24 13:50:08 -0500

    Removed the part reading from local .env file and had docker compose to handle the env

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,16 @@
-require("dotenv").config({
-  path: (() => {
-    switch (process.env.NODE_ENV) {
-      case "test":
-        return ".env.test";
-      case "production":
-        return ".env.prod";
-      case "development":
-      default:
-        return ".env.dev";
-    }
-  })(),
-});
+// require("dotenv").config({
+//   path: (() => {
+//     switch (process.env.NODE_ENV) {
+//       case "test":
+//         return ".env.test";
+//       case "production":
+//         return ".env.prod";
+//       case "development":
+//       default:
+//         return ".env.dev";
+//     }
+//   })(),
+// });
 
 const app = require("./app")
 
