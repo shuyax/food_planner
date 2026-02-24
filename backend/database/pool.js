af3f773182fff7403620c400408335682b8ea6fc
Shuya <syx172524@gmail.com>
2026-02-24 13:49:30 -0500

    Removed the part reading from local .env file and had docker compose to handle the env

--- backend/database/pool.js
+++ backend/database/pool.js
@@ -1,21 +1,21 @@
 const { Pool } = require("pg");
-const path = require("path");
-const dotenv = require("dotenv");
-const envMap = {
-  test: ".env.test",
-  development: ".env.dev",
-  production: ".env.prod",
-};
-const envFileName = envMap[process.env.NODE_ENV] || ".env.dev";
-const envFile = path.resolve(__dirname, `../${envFileName}`);
-dotenv.config({ path: envFile, override: true });
+// const path = require("path");
+// const dotenv = require("dotenv");
+// const envMap = {
+//   test: ".env.test",
+//   development: ".env.dev",
+//   production: ".env.prod",
+// };
+// const envFileName = envMap[process.env.NODE_ENV] || ".env.dev";
+// const envFile = path.resolve(__dirname, `../${envFileName}`);
+// dotenv.config({ path: envFile, override: true });
 
-console.log("💡 Connecting with DB config:", {
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
-  user: process.env.DB_USER,
-});
+// console.log("💡 Connecting with DB config:", {
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   database: process.env.DB_NAME,
+//   user: process.env.DB_USER,
+// });
 
 const pool = new Pool({
   user: process.env.DB_USER,
@@ -25,7 +25,7 @@ const pool = new Pool({
   port: process.env.DB_PORT,
 });
 
-// Optional but VERY helpful during debugging
+
 pool.on("connect", () => {
   console.log(
     `🗄️ Connected to ${process.env.NODE_ENV} database: ${process.env.DB_NAME}`
